bdc187a915f0a5225e0e59cf59fa3d8216c940fa
ZakuHuaso <g.vergara.val@gmail.com>
2023-09-23 19:08:54 -0300

    crea clase libro con atributos

---
+++ LibreriaAteneo/src/com/libreria/Libro.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.libreria;
+
+/**
+ *
+ * @author Estudio
+ */
+public class Libro {
+    private String título, autor, codISBN;
+    private int precio;
+
+}
